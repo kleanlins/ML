cbda5d1f6c4e5fda94c18ddaf5c0c5c7d08ca9ee
Cleanderson Lins <cleandersonlins@gmail.com>
2019-05-24 14:47:03 -0300

    Path renamed

--- Part_1_Data Preprocessing/data_preprocessing_me.py
+++ Part_1_Data Preprocessing/data_preprocessing_me.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # importing the dataset
-dataset = pd.read_csv("Data.csv")
+dataset = pd.read_csv(".Data.csv")
 X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, 3].values
 
