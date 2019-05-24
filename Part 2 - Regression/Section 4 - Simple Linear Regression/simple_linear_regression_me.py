e99bb5de532143c4fe3505bbff835c06618f194f
Cleanderson Lins <cleandersonlins@gmail.com>
2019-05-24 18:44:23 -0300

    Simple Linear Regression with plots

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression_me.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression_me.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 23 14:28:29 2019
+Created on Fri May 24 17:40:30 2019
 
 @author: cleandersonlins
 """
@@ -12,17 +12,17 @@ import pandas as pd
 
 # input variables
 y_pos = -1
-filename = "Data.csv"
+filename = "Salary_Data.csv"
 
 # importing the dataset
-dataset = pd.read_csv(filename)
+dataset = pd.read_csv(filename, encoding='utf-8')
 X = dataset.iloc[:, :y_pos].values
 Y = dataset.iloc[:, y_pos].values
 
 # splitting the dataset into training and test sets
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=1/3, random_state=0)
 
 # feature scaling
 """
@@ -32,3 +32,30 @@ sc_X = StandardScaler().fit(X_train)
 X_train = sc_X.transform(X_train)
 X_test = sc_X.transform(X_test)
 """
+
+# fitting simple linear regression into the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression().fit(X_train, y_train)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
