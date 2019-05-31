f8bec0d74e647078d32a4e0a37996945043b8ce9
Cleanderson Lins <cleandersonlins@gmail.com>
2019-05-31 15:56:32 -0300

    MLR - P-value analysis.

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression_me.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression_me.py
@@ -32,3 +32,33 @@ X = X[:, 1:]
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+# fitting Multiple Linear Regression to the training set
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression().fit(X_train, y_train)
+
+y_pred = reg.predict(X_test)
+
+# building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+X = np.append(np.ones((50,1), dtype=np.float), X, axis=1)
+
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+reg_OLS = sm.OLS(endog=Y, exog=X_opt).fit()
+reg_OLS.summary()
+
+
+# removed the highest P value
+X_opt = X[:, [0, 1, 3, 4, 5]]
+reg_OLS = sm.OLS(endog=Y, exog=X_opt).fit()
+reg_OLS.summary()
+
+
+
+
+
+
+
+
+
+
